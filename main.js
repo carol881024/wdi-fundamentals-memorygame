996f61062ca1ef7cf150ff14ddf44ec2d774d0fd
carolcui <60756197+carol881024@users.noreply.github.com>
2020-02-16 21:39:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 console.log("Up and running!");
-var cards = ['queen', 'queen', 'king', 'king'];
+//create objects for each card
+var cards = [
+
+{	card: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+{	card: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	card: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	card: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-	if (cardsInplay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	}
 	else {
@@ -12,13 +33,33 @@ var checkForMatch = function () {
 }
 
 //function when the user flips a card.
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+    console.log(cards[cardId].card);
+	//add card to array of cards 
+	cardsInPlay.push(cards[cardId].card);
+	//show card's image
+	this.setAttribute('src', cards[cardId].cardImage);
+	//check if two cards played
 	if (cardsInPlay.length === 2) {
-		checkForMatch();}
+		checkForMatch();};
+		//Empty cards in play array for next try
+		cardInPlay = [];
 	}
 
-
-flipCard(0);
-flipCard(2);
+	var createBoard = function() {
+		for (let i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		//Set the src attribute to display the back of card image
+		cardElement.setAttribute('src', 'images/back.png');
+		
+		//Set the data-id attribute with value of index of the card array
+		cardElement.setAttribute('data-id', i);
+		//add event listener when user clicks on a card
+		cardElement.addEventListener('click', flipCard);
+		//append the card to the board
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+//call the createBoard function to create the board
+createBoard();
